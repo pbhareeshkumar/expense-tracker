63b45b6663a00f7cac775e69c63955480c636943
Hareeshkumar <pbhareeshkumar.tmlzs@gmail.com>
2026-07-06 12:00:42 +0530

    Initial commit - expense tracker

---
+++ app.js
@@ -0,0 +1,66 @@
+const API_URL ="http://127.0.0.1:5000"
+
+document.addEventListener("DOMContentLoaded",()=>{
+    loadExpenses();
+    loadTotals();
+});
+
+document.getElementById("expense-form").addEventListener("submit", async function(e) {
+    e.preventDefault(); //to prevent default reloding of page
+
+    const amount = document.getElementById("amount").value;
+    const category = document.getElementById("category").value;
+    const date = document.getElementById("date").value;
+    const note = document.getElementById("note").value;
+
+    await fetch(`${API_URL}/expenses`, { //sends request to Flask backend
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({amount: parseFloat(amount), category, date, note})
+    });
+
+    document.getElementById("expense-form").reset();
+    loadExpenses();
+    loadTotals();
+});
+
+async function loadExpenses(){
+    const response = await fetch(`${API_URL}/expenses`); // await - pauses running till fetched
+    const expenses = await response.json();
+
+    const list = document.getElementById("expense-list");
+    list.innerHTML="";
+
+    expenses.forEach(expense => {
+        const li = document.createElement("li");
+        li.textContent = `${expense.amount} - ${expense.category} (${expense.date}) ${expense.note}`
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.addEventListener("click", () => deleteExpense(expense.id));
+
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+    });
+}
+
+async function deleteExpense(id) {
+  await fetch(`${API_URL}/expenses/${id}`, {
+    method: "DELETE"
+  });
+  loadExpenses(); // refresh list after deleting
+  loadTotals();
+}
+
+async function loadTotals() {
+  const response = await fetch(`${API_URL}/expenses/totals`);
+  const totals = await response.json();
+
+  const list = document.getElementById("totals-list");
+  list.innerHTML = "";
+
+  totals.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = `${item.category}: ₹${item.total}`;
+    list.appendChild(li);
+  });
+}
