a952831c0e95e6528be27ceae652dac6aa141e60
Hareeshkumar <pbhareeshkumar.tmlzs@gmail.com>
2026-07-06 12:00:42 +0530

    Creating db in app.py if not exits

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ import os
 app=Flask(__name__) #application object
 CORS(app)
 
+def init_db():
+    conn = sqlite3.connect("expenses.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount REAL NOT NULL,
+            category TEXT NOT NULL,
+            date TEXT NOT NULL,
+            note TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
 def get_db_connection():
     conn = sqlite3.connect("expenses.db")
     conn.row_factory = sqlite3.Row #makes them behave like dictionaries 
